83b68afdb4f3fb4170240864a42d84f1a995b8ca
Shymaa-ashqar <shymaa.ashqar@gmail.com>
2022-01-30 17:37:09 +0200

    .

--- home/index.php
+++ home/index.php
@@ -20,13 +20,6 @@
   <link href="../assets/css/carousel-recommendation.css" rel="stylesheet">
   <link href="../assets/ionicons-2.0.1/css/ionicons.css" rel="stylesheet">
   <link href='https://fonts.googleapis.com/css?family=Catamaran:400,100,300' rel='stylesheet' type='text/css'>
-
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
 </head>
 
 <body>
@@ -122,56 +115,14 @@
   </header>
   <br><br>
 
-  <!-- <div class="container">
-      <div class="row">
-        <div class="col-sm-3 align-center">
-          <a href="../blog/">
-            <img src="../assets/img/tiles/blog.jpg" alt="Blog" class="image"/>
-          </a>
-          <br><br>
-
-          <a href="../blog/">Blog headlines</a>
-        </div>
-        <div class="col-sm-3 align-center">
-          <a href="#video" data-gallery="#video" data-source="vimeo" data-id="110691368" data-id="110691368" data-title="Apple iPad Air" data-description="So capable, you won’t want to put it down. So thin and light, you won’t have to.">
-            <img src="../assets/img/tiles/video-apple.jpg" alt="New devices" class="image"/>
-          </a>
-          <br><br>
-
-          <a href="#video" data-gallery="#video" data-source="vimeo" data-id="110691368" data-title="Apple iPad Air" data-description="So capable, you won’t want to put it down. So thin and light, you won’t have to.">New apple diveces</a>
-        </div>
-        <div class="col-sm-3 align-center">
-          <a href="#video" data-gallery="#video" data-source="youtube" data-id="6g-ZIm0wge4" data-title="Best New Dell Laptops" data-description="Best of dell's laptops that you can consider buying in 2016. 4 Laptops are featured in the video and all of them has equal importance and there is no order that #1 is better than #2">
-            <img src="../assets/img/tiles/video-dell.jpg" alt="Del XPS" class="image"/>
-          </a>
-          <br><br>
-
-          <a href="#video" data-gallery="#video" data-source="youtube" data-id="6g-ZIm0wge4" data-title="Best New Dell Laptops" data-description="Best of dell's laptops that you can consider buying in 2016. 4 Laptops are featured in the video and all of them has equal importance and there is no order that #1 is better than #2">Brend new DELL XPS</a>
-        </div>
-        <div class="col-sm-3 align-center">
-          <a href="../blog/">
-            <img src="../assets/img/tiles/gallery.jpg" alt="Gallery" class="image"/>
-          </a>
-          <br><br>
-          
-          <a href="../blog/">Products gallery</a>
-        </div>
-      </div>
-    </div> -->
   <hr class="offset-lg">
   <hr class="offset-lg">
-
   <div class="container">
     <h2>NEW PRODUCTS</h2>
-
-
-
     <hr class="offset-md">
     <div class='
         row products'>
       <?php
-
-
       $db_user = "root";
       $db_pass = "";
       $db_name = "tech-life";
@@ -188,13 +139,10 @@
       $row = $stmt->fetchALL(PDO::FETCH_ASSOC);
       foreach ($row
         as $element) { ?>
+        <div class=' col-sm-6 col-md-4 product'>
 
-
-        <div class='
-        col-sm-6 col-md-4 product'>
-
-          <img src=<?php echo $element['image'] ?> alt='product Image' style="width:100%;" /></a>
-          <div class='content' style="width:100%;">
+          <img src=<?php echo $element['image'] ?> alt='product Image' /></a>
+          <div class='content'>
             <h1 class='h4'><?php echo $element['name'] ?></h1>
             <p class="price"><?php
                               if ($element['discount'] != 1) {
@@ -213,73 +161,9 @@
           </div>
         </div>
 
-
-
-
-
       <?php   } ?>
     </div>
   </div>
-
-  <!-- <div class="row products">
-        <div class="col-sm-6 col-md-4 product">
-          <a href="#favorites" class="favorites" data-favorite="inactive"><i class="ion-ios-heart-outline"></i></a>
-          <a href="./"><img src="../assets/img/products/surface-pro.jpg" alt="Surface Pro"/></a>
-
-          <div class="content">
-            <h1 class="h4">Surface Pro</h1>
-            <p class="price">$199.99</p>
-            <label>Hybrid</label>
-
-            <a href="../catalog/product.html" class="btn btn-link"> Details</a>
-            <button class="btn btn-primary btn-rounded btn-sm"> <i class="ion-bag"></i> Add to cart</button>
-          </div>
-        </div>
-
-        <div class="col-sm-6 col-md-4 product">
-          <a href="#favorites" class="favorites" data-favorite="inactive"><i class="ion-ios-heart-outline"></i></a>
-          <a href="./"><img src="../assets/img/products/lenovo-yoga.jpg" alt="Lenovo Yoga"/></a>
-
-          <div class="content">
-            <h1 class="h4">Lenovo Yoga</h1>
-            <p class="price">$199.99</p>
-            <label>Hybrid</label>
-
-            <a href="../catalog/product.html" class="btn btn-link"> Details</a>
-            <button class="btn btn-primary btn-rounded btn-sm"> <i class="ion-bag"></i> Add to cart</button>
-          </div>
-        </div>
-
-        <div class="col-sm-6 col-md-4 product">
-          <a href="#favorites" class="favorites" data-favorite="inactive"><i class="ion-ios-heart-outline"></i></a>
-          <a href="./"><img src="../assets/img/products/asus-transformer.jpg" alt="ASUS Transformer"/></a>
-
-          <div class="content">
-            <h1 class="h4">ASUS Transformer</h1>
-            <p class="price">$199.99</p>
-            <label>Hybrid</label>
-
-            <a href="../catalog/product.html" class="btn btn-link"> Details</a>
-            <button class="btn btn-primary btn-rounded btn-sm"> <i class="ion-bag"></i> Add to cart</button>
-          </div>
-        </div>
-
-        <div class="col-sm-6 col-md-4 product hidden-md hidden-lg">
-          <a href="#favorites" class="favorites" data-favorite="inactive"><i class="ion-ios-heart-outline"></i></a>
-          <a href="./"><img src="../assets/img/products/mi-pad-2.jpg" alt="Mi Pad 2/"></a>
-
-          <div class="content">
-            <h1 class="h4">Mi Pad 2</h1>
-            <p class="sale">$199.99</p>
-            <p class="price through">$249.99</p>
-            <label>Tablets</label>
-
-            <a href="../catalog/product.html" class="btn btn-link"> Details</a>
-            <button class="btn btn-primary btn-rounded btn-sm"> <i class="ion-bag"></i> Add to cart</button>
-          </div>
-        </div>
-      </div>
-    </div> -->
 
   <div class="container">
     <h2>BEST DEALS FOR YOU</h2>
@@ -296,8 +180,8 @@
 
         <div class='col-sm-6 col-md-4 product'>
 
-          <img src=<?php echo $element['image'] ?> alt='product Image' style="width:100%;" />
-          <div class='content' style="width:100%;">
+          <img src=<?php echo $element['image'] ?> alt='product Image' />
+          <div class='content'>
             <h1 class='h4'><?php echo $element['name'] ?></h1>
             <p class="price"><?php
                               if ($element['discount'] != 1) {
@@ -321,129 +205,12 @@
       ?>
     </div>
   </div>
-  <!-- 
-      <div class="row products">
-        <div class="col-sm-6 col-md-3 product">
-          <a href="#favorites" class="favorites" data-favorite="inactive"><i class="ion-ios-heart-outline"></i></a>
-          <a href="./"><img src="../assets/img/products/chrome-book-11.jpg" alt="HP Chromebook 11"/></a>
 
-          <div class="content">
-            <h1 class="h4">HP Chromebook 11</h1>
-            <p class="price">$199.99</p>
-            <label>Laptops</label>
-
-            <a href="../catalog/product.html" class="btn btn-link"> Details</a>
-            <button class="btn btn-primary btn-rounded btn-sm"> <i class="ion-bag"></i> Add to cart</button>
-          </div>
-        </div>
-
-        <div class="col-sm-6 col-md-3 product">
-          <a href="#favorites" class="favorites" data-favorite="inactive"><i class="ion-ios-heart-outline"></i></a>
-          <a href="./"><img src="../assets/img/products/chrome-book-14.jpg" alt="HP Chromebook 14"/></a>
-
-          <div class="content">
-            <h1 class="h4">HP Chromebook 14</h1>
-            <p class="sale">$209.99</p>
-            <p class="price through">$249.99</p>
-            <label>Laptops</label>
-
-            <a href="../catalog/product.html" class="btn btn-link"> Details</a>
-            <button class="btn btn-primary btn-rounded btn-sm"> <i class="ion-bag"></i> Add to cart</button>
-          </div>
-        </div>
-
-        <div class="col-sm-6 col-md-3 product">
-          <a href="#favorites" class="favorites" data-favorite="inactive"><i class="ion-ios-heart-outline"></i></a>
-          <a href="./"><img src="../assets/img/products/chrome-book-asus.jpg" alt="HP Chromebook 14"/></a>
-
-          <div class="content">
-            <h1 class="h4">Asus Chromebook</h1>
-            <p class="price">$299.99</p>
-            <label>Laptops</label>
-
-            <a href="../catalog/product.html" class="btn btn-link"> Details</a>
-            <button class="btn btn-primary btn-rounded btn-sm"> <i class="ion-bag"></i> Add to cart</button>
-          </div>
-        </div>
-
-        <div class="col-sm-6 col-md-3 product">
-          <a href="#favorites" class="favorites" data-favorite="inactive"><i class="ion-ios-heart-outline"></i></a>
-          <a href="./"><img src="../assets/img/products/chrome-book-14.jpg" alt="HP Chromebook 14"/></a>
-
-          <div class="content">
-            <h1 class="h4">HP Chromebook 14</h1>
-            <p class="sale">$209.99</p>
-            <p class="price through">$249.99</p>
-            <label>Laptops</label>
-
-            <a href="../catalog/product.html" class="btn btn-link"> Details</a>
-            <button class="btn btn-primary btn-rounded btn-sm"> <i class="ion-bag"></i> Add to cart</button>
-          </div>
-        </div>
-      </div>
-    </div>
-
-
-    <div class="container">
-      <h2>NEW ARTICLES</h2>
-      <hr class="offset-md">
-
-      <div class="row blog">
-        <div class="col-sm-6 col-md-6 item">
-
-          <div class="body">
-            <a href="../blog/item-photo.html" class="view"><i class="ion-ios-book-outline"></i></a>
-
-            <a href="../blog/item-photo.html">
-              <img src="../assets/img/blog/img1.jpg" title="Apple Devices" alt="Apple Devices">
-            </a>
-
-            <div class="caption">
-              <h1 class="h3">The next generation of Multi-Touch</h1>
-              <label> 07.01.2017</label>
-              <hr class="offset-sm">
-
-              <p>
-                The original iPhone introduced the world to Multi-Touch, forever changing the way people experience technology. With 3D Touch, you can do things that were never possible before. It senses how deeply you press the display, letting you do all kinds of essential things more quickly and simply. And it gives you real-time feedback in the form of subtle taps from the all-new Taptic Engine.
-              </p>
-              <hr class="offset-sm">
-
-              <a href="../blog/item-photo.html"> View article <i class="ion-ios-arrow-right"></i> </a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm-6 col-md-6 item">
-          <div class="body">
-            <a href="../blog/item-video.html" class="view"><i class="ion-ios-book-outline"></i></a>
-            <a href="../blog/item-video.html">
-              <img src="../assets/img/blog/img2.jpg" title="Coffee" alt="Coffee">
-            </a>
-
-            <div class="caption">
-              <h1 class="h3">MacBook Pro - brand new day for business.</h1>
-              <label> 02.01.2017</label>
-              <hr class="offset-sm">
-
-              <p>
-                Organizations everywhere are realizing the potential that Mac brings to their employees by giving them the freedom to use the tools they already know and love. Software and hardware made for each other. Because Apple designs both the software and hardware, every Mac delivers the best possible experience for employees.
-              </p>
-              <hr class="offset-sm">
-
-              <a href="../blog/item-video.html"> View article <i class="ion-ios-arrow-right"></i> </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> -->
   <hr class="offset-lg">
   <hr class="offset-lg">
-
   <?php include '../footer.php' ?>
-
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <script src="../assets/js/jquery-latest.min.js"></script>
-
   <!-- Include all compiled plugins (below), or include individual files as needed -->
   <script src="../assets/js/bootstrap.min.js"></script>
   <script src="../assets/js/core.js"></script>
